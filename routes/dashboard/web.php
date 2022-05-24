930d715d562bab210a40329b2d4c18e2ae6a4631
mohamedelfert <me_me4152@yahoo.com>
2022-05-24 15:18:13 +0200

    create DashboardController.php , create dashboard folder in views and in it create two views (index,empty) , in layouts\dashboard folder edit layouts files , in dashboard\web.php add new route::get('/index').

--- routes/dashboard/web.php
+++ routes/dashboard/web.php
@@ -4,8 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('dashboard')->name('dashboard.')->group(function () {
 
-    Route::get('/check',function (){
-        return "this is dashboard";
-    });
+    Route::get('/index', 'DashboardController@index')->name('index');
 
 });
