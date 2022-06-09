484180c1cdcde4e44c1a9e7d007e51e0225f4d93
medot <mohamedelfert@yahoo.com>
2022-06-09 11:55:05 +0200

    comment sales_data

--- app/Http/Controllers/Dashboard/DashboardController.php
+++ app/Http/Controllers/Dashboard/DashboardController.php
@@ -21,13 +21,13 @@ class DashboardController extends Controller
         $clients_count = Client::count();
         $users_count = User::whereRoleIs('admin')->count();
 
-        $sales_data = Order::select(
-            DB::raw('YEAR(created_at) as year'),
-            DB::raw('MONTH(created_at) as month'),
-            DB::raw('SUM(total_price) as sum'),
-        )->groupBy('month')->get();
+//        $sales_data = Order::select(
+//            DB::raw('YEAR(created_at) as year'),
+//            DB::raw('MONTH(created_at) as month'),
+//            DB::raw('SUM(total_price) as sum'),
+//        )->groupBy('month')->get();
 
         return view('dashboard.welcome', compact('title',
-            'products_count', 'categories_count', 'clients_count', 'users_count', 'sales_data'));
+            'products_count', 'categories_count', 'clients_count', 'users_count'));
     }
 }
